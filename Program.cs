0c840b760d6e83b4d9321b4dc5bbeb135faed44e
irinamedvedeva <irinamedvedeva989@gmail.com>
2016-10-24 13:48:08 -0700

    Initial commit
    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://www.vk.com");
+            driver.Manage().Window.Maximize();
+
+            IWebElement emailTextField = driver.FindElement(By.Id("index_email"));
+            emailTextField.Clear();
+
+            emailTextField.SendKeys("380958368820"); 
+
+            driver.Close();
+            driver.Quit();
+
+
+        }
+    }
+}
